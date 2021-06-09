1624ad01d8c8c21c52647f938c42228061e3fa7d
Guillaume Millet <guillaume@famille-millet.fr>
2021-06-09 08:02:16 +0200

    Fix date display in weather image

--- QWeather/DisplayImageGenerator.cpp
+++ QWeather/DisplayImageGenerator.cpp
@@ -6,6 +6,11 @@ DisplayImageGenerator::DisplayImageGenerator(unsigned int width, unsigned int he
     myWidth(width),
     myHeight(height)
 {
+
+    myFontTemp.setPixelSize(20);
+    myFontTemp.setBold(true);
+
+    myFontDate.setPixelSize(16);
 }
 
 DisplayImageGenerator::~DisplayImageGenerator()
@@ -17,23 +22,16 @@ const QPixmap& DisplayImageGenerator::createWeatherPixmap(const QPixmap& icon, f
 {
     QPixmap scaled = icon.scaledToWidth(myWidth, Qt::SmoothTransformation);
 
-    QFont fontTemp;
-    fontTemp.setPixelSize(20);
-    fontTemp.setBold(true);
-
-    QFont fontDate;
-    fontTemp.setPixelSize(20);
-
     // Weather icon
     myPainter.fillRect(QRect(0,0,myWidth, myHeight), QColor("black") );
     myPainter.drawPixmap(QPoint(0,0), scaled);
 
     // Date of forecast
     myPainter.setPen(QColor("white") );
-    myPainter.setFont(fontDate);
-    myPainter.drawText(QRect(20,scaled.height()-10, myWidth, 30), QString("%1/%2").arg(date.tm_mday).arg(date.tm_mon) );
+    myPainter.setFont(myFontDate);
+    myPainter.drawText(QRect(10,scaled.height()-10, myWidth, 30), QString("%1/%2").arg(date.tm_mday,2,10,QChar('0')).arg(date.tm_mon+1,2,10,QChar('0')) );
 
-    myPainter.setFont(fontTemp);
+    myPainter.setFont(myFontTemp);
 
     // Max temp
     myPainter.setPen(QColor("red") );
